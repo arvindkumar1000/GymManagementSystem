e2c2346c7a0a5c7fa5942a75aebceef64f9a6fb1
arvindkumar1000 <arvindnagar58955@gmail.com>
2025-03-23 14:28:02 +0530

    correction the membership section in modals

--- Routes/gym.js
+++ Routes/gym.js
@@ -1,12 +1,12 @@
-const  express=require("express");
+const express = require("express");
 const router = express.Router();
-const GymController=require('../Controllers/gym')
+const GymController = require('../Controllers/gym')
 
 
-router.post('/register',GymController.register);
-router.post('/login',GymController.login);
-router.post('./reset-password/sendOtp',GymController.sendOtp);
-router.post('/reset-password/checkOtp',GymController.checkOtp);
-router.post('/reset-password',GymController.resetPassword);
+router.post('/register', GymController.register);
+router.post('/login', GymController.login);
+router.post('./reset-password/sendOtp', GymController.sendOtp);
+router.post('/reset-password/checkOtp', GymController.checkOtp);
+router.post('/reset-password', GymController.resetPassword);
 
-module.exports=router;
+module.exports = router;
